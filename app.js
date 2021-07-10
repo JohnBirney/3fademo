a3fa9846a615948efec33a38d50bab58d2c646df
JohnBirney <71044231+JohnBirney@users.noreply.github.com>
2021-07-10 11:09:07 +0100

    commit 7

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars')
 
-app.use(express.urlencoded({ extended: true, limit: '15mb' }))
-app.use(express.json({limit: '15mb'}))
+app.use(express.urlencoded({ extended: true, limit: '5mb' }))
+app.use(express.json({limit: '5mb'}))
 app.use(cookieParser(credentials.cookieSecret))
 app.use(expressSession({
     resave: false,
